c47ac626cbdf0a9ed92f7a7ed2ffcadd3c53c0d7
Dev Patel <56962517+dmp060901@users.noreply.github.com>
2020-08-08 18:53:36 +0530

    New Start

--- DynamicTimeTable/urls.py
+++ DynamicTimeTable/urls.py
@@ -1,4 +1,4 @@
-"""DynamicTimeTable1 URL Configuration
+"""DynamicTimeTable URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
--- DynamicTimeTable/wsgi.py
+++ DynamicTimeTable/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for DynamicTimeTable1 project.
+WSGI config for DynamicTimeTable project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DynamicTimeTable1.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DynamicTimeTable.settings')
 
 application = get_wsgi_application()
